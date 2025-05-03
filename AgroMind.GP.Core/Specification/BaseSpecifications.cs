b247eb2f0d4a07cc79030a2914379a119df6a847
Sara Maged <Saramaged660@gmail.com>
2025-05-03 07:50:37 +0300

    Update ISpecification And Specification For Sorting

--- AgroMind.GP.Core/Specification/BaseSpecifications.cs
+++ AgroMind.GP.Core/Specification/BaseSpecifications.cs
@@ -12,21 +12,39 @@ namespace AgroMind.GP.Core.Specification
 
 
 		public Expression<Func<TEntity, bool>> ?Criteria { get; set; }
+
+		#region Include
 		public List<Expression<Func<TEntity, object>>> Includes { get; set; } = new List<Expression<Func<TEntity, object>>>();
 
 
-		//Get All
-		public BaseSpecifications()
+		protected void AddInclude(Expression<Func<TEntity, object>> includeExpression)
+
+				=> Includes.Add(includeExpression);
+
+		#endregion
+
+		#region Sorting
+		public Expression<Func<TEntity, object>> OrderBy { get; private set; }
+		public Expression<Func<TEntity, object>> OrderByDescending { get; private set; }
+
+		protected void AddOrderBy(Expression<Func<TEntity, object>> orderByExpression)=>OrderBy= orderByExpression;
+
+		protected void AddOrderByDescending(Expression<Func<TEntity, object>> orderByDescExpression) => OrderByDescending = orderByDescExpression;
+	    #endregion
+
+	    //Get All
+	    public BaseSpecifications()
 		{
 			//Includes = new List<Expression<Func<TEntity, object>>>();
 		}
 
 		//Get By ID
-		public BaseSpecifications(Expression<Func<TEntity, bool>> criteriaExpression)
+		public BaseSpecifications(Expression<Func<TEntity, bool>>? criteriaExpression)
 		{
 			Criteria = criteriaExpression;
 			//Includes=new List<Expression<Func<TEntity,object>>>();	
 		}
 
+		
 	}
 }
