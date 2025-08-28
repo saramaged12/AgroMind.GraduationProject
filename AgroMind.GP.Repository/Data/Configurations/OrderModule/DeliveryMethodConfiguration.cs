6938b1cd2da7a29cf500032e4478a71113200225
Sara Maged <Saramaged660@gmail.com>
2025-08-28 03:41:07 +0300

    OrderConfigurations

---
+++ AgroMind.GP.Repository/Data/Configurations/OrderModule/DeliveryMethodConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AgroMind.GP.Core.Entities.Orders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroMind.GP.Repository.Data.Configurations.OrderModule
+{
+	public class DeliveryMethodConfiguration : IEntityTypeConfiguration<DeliveryMethod>
+	{
+		public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<DeliveryMethod> builder)
+		{
+			builder.Property(deliveryMethod => deliveryMethod.Cost)
+				.HasColumnType("decimal(8,2)");
+		}
+	}
+}
